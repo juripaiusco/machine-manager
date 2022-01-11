effd47733febcb0f6ccd6092c5472b6e0dd2bcdf
Juri <juri@mr-j.it>
2022-01-11 22:22:13 +0100

    Dividere parte anteriore con posteriore (pagina 1 e pagina 2)

--- resources/views/machines/form-dynamic-field.blade.php
+++ resources/views/machines/form-dynamic-field.blade.php
@@ -1,44 +1,59 @@
-<div class="row">
+@foreach($fields_obj as $section)
 
-    @foreach($fields_obj as $field)
+    <div class="card">
+        <div class="card-header">
+            <strong>{{ $section->title }}</strong>
+        </div>
+        <div class="card-body">
 
-        @php
-            $field_name = preg_replace("/[^a-zA-Z0-9_]+/", "", strtolower(str_replace(' ', '_', $field->name)))
-        @endphp
+            <div class="row">
 
-        <div class="col-lg-6">
+                @foreach($section->fields as $field)
 
-            @switch($field->type)
+                    @php
+                        $field_name = preg_replace("/[^a-zA-Z0-9_]+/", "", strtolower(str_replace(' ', '_', $field->name)))
+                    @endphp
 
-                @case('check')
+                    <div class="col-lg-6">
 
-                <div class="custom-control custom-switch">
-                    <input type="checkbox"
-                           class="custom-control-input"
-                           id="{{ $field_name }}"
-                           name="json[{{ $field_name }}]"
-                           @if(isset($json[$field_name]) && $json[$field_name] == 'on')
-                           checked
-                        @endif>
-                    <label class="custom-control-label"
-                           for="{{ $field_name }}">
-                        {{ $field->name }}
-                    </label>
-                </div>
+                        @switch($field->type)
 
-                @break
+                            @case('check')
 
-                @default
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox"
+                                       class="custom-control-input"
+                                       id="{{ $field_name }}"
+                                       name="json[{{ $field_name }}]"
+                                       @if(isset($json[$field_name]) && $json[$field_name] == 'on')
+                                       checked
+                                    @endif>
+                                <label class="custom-control-label"
+                                       for="{{ $field_name }}">
+                                    {{ $field->name }}
+                                </label>
+                            </div>
 
-                <x-form-group-text
-                    :fieldName="$field_name"
-                    :field="$field"
-                    :json="$json ?? ''" />
+                            @break
 
-            @endswitch
+                            @default
+
+                            <x-form-group-text
+                                :fieldName="$field_name"
+                                :field="$field"
+                                :json="$json ?? ''" />
+
+                        @endswitch
+
+                    </div>
+
+                @endforeach
+
+            </div>
 
         </div>
+    </div>
 
-    @endforeach
+    <br>
 
-</div>
+@endforeach
